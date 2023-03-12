2755a09eb38ba3bfbe591d79048bd03448ad5713
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2023-03-12 11:31:50 -0400

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Stewart Chan on 2023-03-12.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
